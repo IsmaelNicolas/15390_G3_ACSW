18a3cebb41c72c782d26ebeabb1c478740194fcf
IsmaelNicolas <ismael.cedillo@outlook.es>
2024-01-16 22:06:12 -0500

    Set protected routes to home

--- CODE/front/src/routes/home/+layout.svelte
+++ CODE/front/src/routes/home/+layout.svelte
@@ -1,10 +1,8 @@
 <script lang="ts">
-	import '../../app.css';
 	import Header  from '../../components/NavBar.svelte';
-
 </script>
 
-<div class="m-0 bg-white dark:bg-gray-800 h-dvh">
+<div class="m-0 bg-white dark:bg-gray-800 h-auto">
 	<Header/>
 	<main class="m-0">
 		<slot />
--- CODE/front/src/routes/home/+page.svelte
+++ CODE/front/src/routes/home/+page.svelte
@@ -1,7 +1,11 @@
 <script lang="ts">
+	import { goto } from '$app/navigation';
 	import { onMount } from 'svelte';
 	import TableHistory from '../../components/TableHistory.svelte';
 	import UploadImage from '../../components/UploadImage.svelte';
+	import Loader from '../../components/Loader.svelte';
+	import {getCookie,setCookie} from '../../Utils/function'
+
 	// Definir el tipo para un evento
 	interface Event {
 		fecha: string;
@@ -12,18 +16,38 @@
 	let eventos: Event[] = [];
 
 	async function load_histories() {
-		const response = await fetch('http://localhost:8000/historias');
+		const access_token = getCookie('access_token');
+		const response = await fetch('http://localhost:8000/historias', {
+			headers: {
+				Authorization: `Bearer ${access_token}`
+			}
+		});
 		if (response.ok) {
 			const data = await response.json();
 			console.log(data); // Verify that the data is logged correctly
 			eventos = data; // Assign the data to the eventos variable
 			return data;
+		} else if (response.status === 401) {
+			goto('/login');
 		} else {
 			console.error('Error al obtener datos de la API');
 			return [];
 		}
 	}
-	
+
+	onMount(async () => {
+		const access_token = getCookie('access_token');
+		console.log(access_token);
+		const response = await fetch('http://localhost:8000/api/user/me', {
+			headers: {
+				Authorization: `Bearer ${access_token}`
+			}
+		});
+		console.log(response);
+		if (!response.ok) {
+			goto('/login');
+		}
+	});
 </script>
 
 <svelte:head>
@@ -32,12 +56,12 @@
 </svelte:head>
 
 <div class="h-screen overflow-hidden">
-	<section class="mx-auto w-3/4   md:h-1/2 flex justify-center items-center">
+	<section class="mx-auto w-3/4 md:h-1/2 flex justify-center items-center">
 		<UploadImage />
 	</section>
-	<section class="w-11/12 mx-auto  ">
+	<section class="w-11/12 mx-auto">
 		{#await load_histories()}
-			<p>Cargando eventos...</p>
+			<Loader />
 		{:then events}
 			{#if events.length > 0}
 				<TableHistory {events} />
