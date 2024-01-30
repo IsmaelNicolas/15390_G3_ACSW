86d4af0730bd4063a4c2c024104f686a9a3b6415
IsmaelNicolas <ismael.cedillo@outlook.es>
2024-01-30 10:22:07 -0500

    Set colors to table

--- CODE/front/src/components/TableHistory.svelte
+++ CODE/front/src/components/TableHistory.svelte
@@ -5,6 +5,7 @@
 		blanco_biologico: string;
 	}
 	export let events: Event[] = [];
+	import Eye from '../components/Eye.svelte';
 </script>
 
 <div class="relative overflow-x-auto">
@@ -14,7 +15,7 @@
 				<th scope="col" class="px-6 py-3"> Fecha </th>
 				<th scope="col" class="px-6 py-3"> Intensidad </th>
 				<th scope="col" class="px-6 py-3"> Blanco biologico </th>
-				<th scope="col" class="px-6 py-3"> Accion </th>
+				<th scope="col" class="px-6 py-3"> </th>
 			</tr>
 		</thead>
 		<tbody>
@@ -26,9 +27,45 @@
 					>
 						{fecha}
 					</th>
-					<td class="px-6 py-4"> {intensidad} </td>
-					<td class="px-6 py-4"> {blanco_biologico} </td>
-					<td class="px-6 py-4"> Borrar </td>
+					{#if blanco_biologico === 'healthy'}
+						<td colspan="2" class="px-6 py-4 text-center">
+							<span
+								class="bg-green-100 text-green-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-green-400 border border-green-400"
+								>Saludable</span
+							>
+						</td>
+					{:else}
+						<td class="px-6 py-4">
+							{#if intensidad === 'severe'}
+								<span
+									class="bg-red-100 text-red-800 text-sm font-medium me-2 px-2.5 py-0.5 rounded dark:bg-red-900 dark:text-red-300"
+									>{intensidad}</span
+								>
+							{/if}
+							{#if intensidad === 'moderate'}
+								<span
+									class="bg-yellow-100 text-yellow-800 text-sm font-medium me-2 px-2.5 py-0.5 rounded dark:bg-yellow-900 dark:text-yellow-300"
+									>{intensidad}</span
+								>
+							{/if}
+						</td>
+						<td class="px-6 py-4">
+							{#if blanco_biologico === 'botrytis'}
+								Botritys
+							{/if}
+							{#if blanco_biologico === 'oidio'}
+								Oidio
+							{/if}
+						</td>
+					{/if}
+					<td class="px-2 py-1">
+						<button
+							class="inline-flex items-center font-medium text-blue-600 dark:text-blue-500 hover:underline"
+						>
+							<Eye clss="w-8 h-6" />
+							Ver
+						</button>
+					</td>
 				</tr>
 			{/each}
 		</tbody>
